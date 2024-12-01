1ff6e113a6c86ee05325c249988b3f420d19a55e
0cRAY <166521292+imrayyarmi@users.noreply.github.com>
2024-12-01 21:29:28 +0800

    Update a1. Dice Combinations.cpp

--- CSES/a3. Dynamic Programming/a1. Dice Combinations.cpp
+++ CSES/a3. Dynamic Programming/a1. Dice Combinations.cpp
@@ -1,12 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
-dp[i] 為數字 i 的組合方式
-如果最一顆骰子為 1，那他組合數便包含 dp[i - 1] (i = (i - 1) + 1)
-故dp[i] = dp[i - n]的加總，其中 n = 1~6
-*/
-
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
